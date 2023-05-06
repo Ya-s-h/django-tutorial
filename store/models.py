958d648157918defc1dc35747eccf7fb0cabdcef
Yash Aggarwal <vasu2013agg@gmail.com>
2023-05-07 04:55:05 +0530

    Creating models and migrations

--- store/models.py
+++ store/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Store(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    phone = models.CharField(max_length=10)
+    email = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- storefront/settings.py
+++ storefront/settings.py
@@ -34,12 +34,12 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    # 'django.contrib.sessions',
+    'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'playground',
     'debug_toolbar',
-    'store',
+    'store.apps.StoreConfig',
     'tags'
 ]
 
